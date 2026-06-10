3ceb6e05e292eefdd130db909d01cfab55df3b70
Bảo Ngọc <89443501+ngoc15025@users.noreply.github.com>
2026-06-10 11:27:39 +0700

    Update build.sh

--- QRDrinkOrder/build.sh
+++ QRDrinkOrder/build.sh
@@ -5,8 +5,10 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 
-# 2. Xóa sạch các thư mục rác (bin, obj) từ Windows
-find . -type d \( -name "bin" -o -name "obj" \) -prune -exec rm -rf {} +
+# 2. Xóa DỨT ĐIỂM các file rác bằng nhiều cách để đảm bảo 100% sạch sẽ
+rm -rf */obj */bin
+rm -rf obj bin
+./dotnet/dotnet clean QRDrinkOrder.Client/QRDrinkOrder.Client.csproj
 
 # 3. Build project
 ./dotnet/dotnet publish QRDrinkOrder.Client/QRDrinkOrder.Client.csproj -c Release -o output
